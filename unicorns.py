7f450dd81d1206a6394c54f47aca72405fa0902d
cameronreaves <cameronreaves@dhcp-10-250-12-184.harvard.edu>
2020-02-12 21:34:22 -0500

    added card functions, still need to do magic and instant, fixed new turn bug

--- unicorns.py
+++ unicorns.py
@@ -1,7 +1,7 @@
 import random
 
 #set rules
-start_hand = 10
+start_hand = 3
 card_types = [1, 2, 3, 4, 5]
 size = 50
 
@@ -137,13 +137,23 @@ class Player:
         for s in self.my_stable.stable:
             print(s)
 
-    def count_uni_stable(self):
-        count = 0
+    def check_stable(self):
+        types = ["Unicorn", "Upgrade", "Downgrade"]
+        u_count = 0
+        up_count = 0
+        down_count = 0
         for s in self.my_stable.stable:
-            if str(s) == "Unicorn":
-                count += 1
-        return count
+            if str(s) == types[0]:
+                u_count += 1
+            elif str(s) == types[1]:
+                up_count += 1
+            else:
+                down_count += 1
+        counts = [u_count, up_count, down_count]
+        return counts
 
+
+#   goes through a players hand and looks for that card, deletes that card from the hand and then returns it
     def check_and_take(self, card_name):
         for i in range(len(self.hand)):
             if self.hand[i].get_name() == card_name:
@@ -153,9 +163,21 @@ class Player:
 
     def steal_my_from_stable(self,card_name):
         if len(self.my_stable.stable) > 0:
-            main_trash.to_trash(self.my_stable.stable.pop())
-            return True
+            for i in range(len(self.my_stable.stable)):
+                if self.my_stable.stable[i].get_name() == card_name:
+                    card = self.my_stable.stable[i]
+                    del self.my_stable.stable[i]
+                    main_trash.to_trash(card)
+                    return True
         return False
+
+    def instant_play(self):
+        main_trash.to_trash(self.my_stable.stable.pop())
+
+    def reduce_hand(self):
+        limit = 3
+        self.hand[0:limit]
+
 
 
 
@@ -173,46 +195,69 @@ players.append(Player("Cam"))
 players.append(Player("Naomi"))
 run = True
 
-players[0].show_hand()
-players[1].show_hand()
-players[1].put_stable("Unicorn")
+# players[1].show_hand()
+# print("\n")
+# players[1].put_stable("Unicorn")
+# players[1].show_stable()
+# bool = players[1].steal_my_from_stable("Unicorn")
+# if bool:
+#     print("worked")
+# else:
+#     print("not famo")
+# print("\n")
+# players[1].show_stable()
+# main_trash.show_trash()
+
+
+
 
 
 # print("""
 # Welcome to Unicorns!
 # The Goal of the Game is to get X unicorns in your stable.
-# Magic cards take away an opponent's unicorn from their stable
-# Upgrade cards give you an extra turn
-# Downgrade cards take away your opponents turn
-# Instant cards do nothing!
+# Magic cards take away an opponent's unicorn from their stable 
+# Upgrade cards give you an card each turn
+# Downgrade cards reduce your opponents hand to 5 cards
+# Instant cards block your opponents card that they just played
 # """)
 #
-# while(run):
-# #for each player in the game
-#     for i in range(len(players)):
-#         turn = True
-#         ##turn
-#         while(turn):
-#             ply = players[i]
-#             print("It's your turn,  " + str(ply))
-#             print("Here is your hand \n")
-#             ply.show_hand()
-#             print("Here is your stable \n")
-#             ply.show_stable()
-#             action = input("\n Type card name or type draw ----->  ")
-#             if action == "draw":
-#                 ply.draw_deck()
-#             else:
-#                 suc = ply.put_stable(action)
-#                 if suc:
-#                     print("You played the " + action + " card to your stable")
-#                     turn = False
-#                 else:
-#                     print("Card not in your hand")
-#         unis = ply.count_uni_stable()
-#         if unis > 2:
-#             print(str(ply) + " is the winner")
-#             run = False
+while(run):
+#for each player in the game
+    for i in range(len(players)):
+        turn = True
+        # makes sure that the player doesn't draw again if she inputs the wrong card name
+        new_turn = True
+        ##turn
+        while(turn):
+            ply = players[i]
+            print("It's your turn,  " + str(ply))
+            if new_turn:
+                ply.draw_deck()
+                for i in range(ply.check_stable()[1]):
+                    print("Upgrade, drawing again!")
+                    ply.draw_deck()
+            print("\n HAND")
+            ply.show_hand()
+            print("\n STABLE")
+            ply.show_stable()
+            action = input("\n Type card name or type draw ----->  ")
+            if action == "draw":
+                ply.draw_deck()
+            else:
+                suc = ply.put_stable(action)
+                if suc:
+                    print("You played the " + action + " card to your stable")
+                    if ply.check_stable()[2] > 0:
+                        print("You have a downgrade card! Reducing your hand.")
+                        ply.reduce_hand()
+                    turn = False
+                else:
+                    print("Card not in your hand")
+                    new_turn = False
+        unis = ply.check_stable()
+        if unis[0] > 2:
+            print(str(ply) + " is the winner")
+            run = False
 
 
 
