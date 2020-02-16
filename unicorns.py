e8221ea405f13c40963ff95af63f46952440807e
cameronreaves <cameronreaves@dhcp-10-250-157-123.harvard.edu>
2020-02-15 20:03:50 -0500

    fixed bug with the downgrade cards, and glitch to end the game with unicorns

--- unicorns.py
+++ unicorns.py
@@ -138,7 +138,7 @@ class Player:
             print(s)
 
     def check_stable(self):
-        types = ["Unicorn", "Upgrade", "Downgrade"]
+        types = ["Unicorn", "Upgrade", "Downgrade", "Magic", "Instant"]
         u_count = 0
         up_count = 0
         down_count = 0
@@ -147,7 +147,7 @@ class Player:
                 u_count += 1
             elif str(s) == types[1]:
                 up_count += 1
-            else:
+            elif str(s) == types[2]:
                 down_count += 1
         counts = [u_count, up_count, down_count]
         return counts
@@ -175,8 +175,8 @@ class Player:
         main_trash.to_trash(self.my_stable.stable.pop())
 
     def reduce_hand(self):
-        limit = 3
-        self.hand[0:limit]
+        limit = 1
+        self.hand = self.hand[0:limit]
 
 
 
@@ -194,7 +194,7 @@ players = []
 players.append(Player("Cam"))
 players.append(Player("Naomi"))
 run = True
-
+down = False
 # players[1].show_hand()
 # print("\n")
 # players[1].put_stable("Unicorn")
@@ -209,6 +209,14 @@ run = True
 # main_trash.show_trash()
 
 
+# players[0].show_hand()
+# players[0].put_stable("Downgrade")
+# players[0].show_stable()
+# print(players[0].check_stable()[2])
+# players[0].steal_my_from_stable("Downgrade")
+# players[0].show_stable()
+# print(players[0].check_stable()[2])
+
 
 
 
@@ -216,11 +224,11 @@ run = True
 # Welcome to Unicorns!
 # The Goal of the Game is to get X unicorns in your stable.
 # Magic cards take away an opponent's unicorn from their stable 
-# Upgrade cards give you an card each turn
-# Downgrade cards reduce your opponents hand to 5 cards
+# Upgrade cards give you two extra cards your next turn
+# Downgrade cards reduce your opponents hand to 3 cards
 # Instant cards block your opponents card that they just played
-# """)
-#
+#""")
+
 while(run):
 #for each player in the game
     for i in range(len(players)):
@@ -236,6 +244,13 @@ while(run):
                 for i in range(ply.check_stable()[1]):
                     print("Upgrade, drawing again!")
                     ply.draw_deck()
+                    ply.draw_deck()
+                    ply.steal_my_from_stable("Upgrade")
+                if down:
+                    print("\n Your opponent played a downgrade card! Reducing your hand.")
+                    ply.reduce_hand()
+                    down = False
+
             print("\n HAND")
             ply.show_hand()
             print("\n STABLE")
@@ -243,13 +258,17 @@ while(run):
             action = input("\n Type card name or type draw ----->  ")
             if action == "draw":
                 ply.draw_deck()
+                turn = False
             else:
                 suc = ply.put_stable(action)
                 if suc:
                     print("You played the " + action + " card to your stable")
                     if ply.check_stable()[2] > 0:
-                        print("You have a downgrade card! Reducing your hand.")
-                        ply.reduce_hand()
+                        # print("You have a downgrade card! Reducing your hand.")
+                        # ply.reduce_hand()
+                        print(action)
+                        down = True
+                        ply.steal_my_from_stable("Downgrade")
                     turn = False
                 else:
                     print("Card not in your hand")
@@ -258,6 +277,7 @@ while(run):
         if unis[0] > 2:
             print(str(ply) + " is the winner")
             run = False
+            break
 
 
 
